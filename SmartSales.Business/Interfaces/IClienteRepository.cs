b33376b9fb4dfb99928d38d2bbed96ea15be6eff
Yehison <yehison.v.c2004@gmail.com>
2026-02-25 21:44:35 -0400

    Creación de clases en la capa de data (Repository), creación de interfaces en la carpeta de interfaces y la creación de las clases services en la carpeta services

---
+++ SmartSales.Business/Interfaces/IClienteRepository.cs
@@ -0,0 +1,16 @@
+﻿using SmartSales.Business.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SmartSales.Business.Interfaces
+{
+    public interface IClienteRepository
+    {
+        public void CrearCliente();
+        public void ActualizarCliente();
+        public void EliminarCliente();
+        public List<Cliente> MostrarClientes();
+
+    }
+}
---
+++ SmartSales.Business/Interfaces/IDetalleVentaRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SmartSales.Business.Interfaces
+{
+    internal interface IDetalleVentaRepository
+    {
+    }
+}
